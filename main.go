b9a02824202c224a9f1f68978117af8917402a60
Mr.Cheen <sda8989bkru@gmail.com>
2024-12-19 09:52:17 +0400

    fix04

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
+// Package main provides the entry point for the database editor application
 package main
 
 import (
 	"log"
+	"os"
 
 	"fyne.io/fyne/v2/app"
 	"github.com/Mr-Cheen1/go-reg/storage"
@@ -11,6 +13,7 @@ import (
 func main() {
 	myApp := app.NewWithID("com.mr-cheen1.go-reg")
 	myApp.Preferences().SetString("theme", "dark")
+	
 	window := myApp.NewWindow("Редактор базы данных")
 
 	excelStorage := storage.NewExcelStorage()
@@ -18,7 +21,8 @@ func main() {
 
 	mainWindow := ui.NewMainWindow(window, excelStorage)
 	if err := mainWindow.Initialize(); err != nil {
-		log.Println("Ошибка инициализации:", err)
+		log.Printf("Ошибка инициализации: %v\n", err)
+		os.Exit(1)
 	}
 
 	window.ShowAndRun()
